4c0d9c6799f4785f37421c61de7d85081df38787
wwdd23 <dancerwudi@gmail.com>
2015-01-22 19:33:32 +0800

    add code into sendmail send ok

---
+++ sendmail.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib
+
+#创建一个带附件的实例
+msg = MIMEMultipart()
+
+#构造附件1
+##att1 = MIMEText(open('d:\\123.rar', 'rb').read(), 'base64', 'gb2312')
+#att1["Content-Type"] = 'application/octet-stream'
+#att1["Content-Disposition"] = 'attachment; filename="123.doc"'#这里的filename可以任意写，写什么名字，邮件中显示什么名字
+#msg.attach(att1)
+#
+##构造附件2
+#att2 = MIMEText(open('d:\\123.txt', 'rb').read(), 'base64', 'gb2312')
+#att2["Content-Type"] = 'application/octet-stream'
+#att2["Content-Disposition"] = 'attachment; filename="123.txt"'
+#msg.attach(att2)
+
+#加邮件头
+msg['from'] = 'wwdd.23@163.com'
+msg['to'] = 'di.wu@i-soft.com.cn'
+msg['subject'] = 'hello world'
+#发送邮件
+try:
+    server = smtplib.SMTP()
+    server.connect('smtp.163.com')
+    server.login('wwdd.23','xxxx')#XXX为用户名，XXXXX为密码
+    server.sendmail(msg['from'], msg['to'],msg.as_string())
+    server.quit()
+    print '发送成功'
+
+except Exception, e:  
+    print str(e) 
